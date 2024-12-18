38d5c229c86e45e42efb2125d4fca56dda5f38d1
parasavaishnavi482000 <parasavaishnavi482000@gmail.com>
2024-12-18 15:37:18 +0530

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { MyHeadComponent } from './my-head/my-head.component';
 import { MyBodyComponent } from './my-body/my-body.component';
 import { MyFootComponent } from './my-foot/my-foot.component';
+import { MyFileComponent } from './my-file/my-file.component';
 @Component({
   selector: 'app-root',
-  imports: [MyHeadComponent,MyBodyComponent,MyFootComponent],
+  imports: [MyHeadComponent,MyBodyComponent,MyFootComponent,MyFileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
