f4dc28814ec15f162d5de8257eb7c0c6f91815b9
parasavaishnavi482000 <parasavaishnavi482000@gmail.com>
2024-12-18 09:26:52 +0530

    foot added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyHeadComponent } from './my-head/my-head.component';
 import { MyBodyComponent } from './my-body/my-body.component';
+import { MyFootComponent } from './my-foot/my-foot.component';
 @Component({
   selector: 'app-root',
-  imports: [MyHeadComponent,MyBodyComponent],
+  imports: [MyHeadComponent,MyBodyComponent,MyFootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
