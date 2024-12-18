5acfc139ebe3e3dbd78e247a12dd285d503863ad
chandra90901 <chandraofficial1316@gmail.com>
2024-12-18 09:28:54 +0530

    code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyHeadComponent } from './my-head/my-head.component';
 import { MyBodyComponent } from './my-body/my-body.component';
+import { MymgrComponent } from './my-mgr/my-mgr.component';
 @Component({
   selector: 'app-root',
-  imports: [MyHeadComponent,MyBodyComponent],
+  imports: [MyHeadComponent, MyBodyComponent, MymgrComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/my-mgr/my-mgr.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-mgr",
+    template: "<div style='color:red'>this my class</div>"
+})
+export class MymgrComponent {
+
+}
