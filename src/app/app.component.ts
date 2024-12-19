2e19c7df555ca7c9a3842db8b880c14efaaaab12
loka2111 <lokanagadatta.beluguri@gmail.com>
2024-12-19 09:27:59 +0530

    mergeFix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MyHeadComponent } from './my-head/my-head.component';
 import { MyBodyComponent } from './my-body/my-body.component';
 import { MymgrComponent } from './my-mgr/my-mgr.component';
-<<<<<<< HEAD
-import { MyBodyLokaComponent } from './my_dodyLoka/my_body_loka_component';
-=======
->>>>>>> d71a2057f56e3096d2b5bcc73738125de3b62e77
+import { MyBodyLokaComponent } from './my_bodyLoka/my_body_loka_component';
 import { MyFootComponent } from './my-foot/my-foot.component';
 import { MyFileComponent } from './my-file/my-file.component';
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-  imports: [MyHeadComponent, MyBodyComponent, MymgrComponent,MyBodyLokaComponent,MyFootComponent],
-=======
-<<<<<<< HEAD
-  imports: [MyHeadComponent,MyBodyComponent,MyFootComponent,MyFileComponent],
-=======
-  imports: [MyHeadComponent, MyBodyComponent, MymgrComponent, MyFootComponent],
->>>>>>> f6e7eaa4825ed59c843a632cb066cd402eea5bc8
->>>>>>> d71a2057f56e3096d2b5bcc73738125de3b62e77
+  imports: [MyHeadComponent, MyBodyComponent, MymgrComponent,MyBodyLokaComponent,MyFootComponent,MyFileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/my-foot/my-foot.component.ts
+++ src/app/my-foot/my-foot.component.ts
@@ -4,10 +4,4 @@ import { Component } from "@angular/core";
     selector: "my-foot",
     template: "<div style='color:red'>this is my footer</div>"
 })
-<<<<<<< HEAD
  export class MyFootComponent{}
-=======
-export class MyFootComponent {
-
-}
->>>>>>> d71a2057f56e3096d2b5bcc73738125de3b62e77
