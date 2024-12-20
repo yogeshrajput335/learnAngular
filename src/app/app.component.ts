d7d70a7f90d2f410f68cb877183da34d8383f59f
parasavaishnavi482000 <parasavaishnavi482000@gmail.com>
2024-12-20 09:25:40 +0530

    files done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,10 @@ import { MymgrComponent } from './my-mgr/my-mgr.component';
 import { MyBodyLokaComponent } from './my_bodyLoka/my_body_loka_component';
 import { MyFootComponent } from './my-foot/my-foot.component';
 import { MyFileComponent } from './my-file/my-file.component';
+import { MyLaptopComponent } from './my-laptop/my-laptop.component';
 @Component({
   selector: 'app-root',
-  imports: [MyHeadComponent, MyBodyComponent, MymgrComponent,MyBodyLokaComponent,MyFootComponent,MyFileComponent],
+  imports: [MyHeadComponent, MyBodyComponent, MymgrComponent,MyBodyLokaComponent,MyFootComponent,MyFileComponent,MyLaptopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
