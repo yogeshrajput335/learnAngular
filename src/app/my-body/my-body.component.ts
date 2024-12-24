259ee6cac2f6c53618fca2c0a1f627e8a963595f
yogesh <yogesh.rajput@pfgc.com>
2024-12-24 10:36:37 +0530

    Observable demo anf consume rest api

--- src/app/my-body/my-body.component.ts
+++ src/app/my-body/my-body.component.ts
@@ -1,12 +1,12 @@
 import { Component } from "@angular/core";
 import { MyAdsComponent } from "../my-ads/my-ads.component";
-import { CommonModule } from "@angular/common";
+import { CommonModule, TitleCasePipe } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: 'my-b',
     templateUrl: './my-body.component.html',
-    imports: [CommonModule,FormsModule],
+    imports: [CommonModule,FormsModule,TitleCasePipe],
     styleUrl: './my-body.component.css'
 })
 export class MyBodyComponent {
@@ -15,6 +15,8 @@ export class MyBodyComponent {
     hasMyClass=true;
     list=['apple','mango','kiwi','banana']
     myColor="green"
+    pipeDemo= "This is for pipe demo"
+    purchasedOn = '2024-07-08';
     ChangeShow(){
         this.isShow = !this.isShow;
         this.hasMyClass = !this.hasMyClass;
---
+++ src/app/test-observable/test-observable.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { interval, of } from 'rxjs';
+
+@Component({
+  selector: 'app-test-observable',
+  imports: [],
+  templateUrl: './test-observable.component.html',
+  styleUrl: './test-observable.component.css'
+})
+export class TestObservableComponent {
+   observable = interval(3000);
+   user1:any
+   user2:any
+   user3:any
+  obs1(){
+    this.user1 = this.observable.subscribe(data=>console.log('observer 1 :',data))
+  }
+  obs2(){
+    this.user2 = this.observable.subscribe(data=>console.log('observer 2 :',data))
+  }
+  obs3(){
+    this.user3 = this.observable.subscribe(data=>console.log('observer 3 :',data))
+  }
+  uobs1(){
+    this.user1.unsubscribe();
+  }
+  uobs2(){
+    this.user2.unsubscribe();
+  }
+  uobs3(){
+    this.user3.unsubscribe();
+  }
+  
+}
